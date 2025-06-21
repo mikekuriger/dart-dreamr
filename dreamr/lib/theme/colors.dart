d1853a1fb6cc5bfb7bb5896ce5c2cefd46bb3b58
Michael Kuriger <mikekuriger@gmail.com>
2025-06-21 10:04:33 -0700

    updates

---
+++ dreamr/lib/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const purple200 = Color(0xFFDDD6FE);
+  static const purple400 = Color(0xFFC084FC);
+  static const purple600 = Color(0xFF9333EA);
+  static const purple700 = Color(0xFF7E22CE);
+  static const purple800 = Color.fromARGB(255, 91, 4, 167);
+  static const purple900 = Color.fromARGB(255, 51, 0, 95);
+}
