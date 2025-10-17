2630f1f5ade3552470d5381bd8c0f3aee556b4e0
Michael Kuriger <mikekuriger@gmail.com>
2025-10-17 10:46:33 -0700

    updates to help support easier logins

--- dreamr/lib/services/dio_client.dart
+++ dreamr/lib/services/dio_client.dart
@@ -1,4 +1,5 @@
 // services/dio_client.dart
+import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:cookie_jar/cookie_jar.dart';
@@ -8,19 +9,28 @@ import 'package:dreamr/constants.dart';
 class DioClient {
   static late final Dio dio;
   static PersistCookieJar? _cookieJar;
+  static bool _initialized = false;
 
   static Future<void> init() async {
+    if (_initialized) return;
+    _initialized = true;
+
     final appDocDir = await getApplicationDocumentsDirectory();
 
-    // store the jar so we can wipe it on logout
     _cookieJar = PersistCookieJar(
       storage: FileStorage('${appDocDir.path}/.cookies/'),
+      ignoreExpires: false, // keep Flask-Login "remember" cookie until it expires
     );
 
     dio = Dio(BaseOptions(
       baseUrl: AppConfig.baseUrl,
-      headers: {'Content-Type': 'application/json'},
+      headers: {
+        HttpHeaders.contentTypeHeader: 'application/json',
+        HttpHeaders.acceptHeader: 'application/json',
+      },
       validateStatus: (status) => status != null && status < 500,
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 20),
     ));
 
     dio.interceptors.add(CookieManager(_cookieJar!));
@@ -34,5 +44,12 @@ class DioClient {
   // (optional) if you ever set an auth header, clear it too
   static void removeAuthHeader() {
     dio.options.headers.remove('Authorization');
+  }
+
+  static Dio get instance {
+    if (!_initialized) {
+      throw StateError('DioClient.init() must be called before use.');
+    }
+    return dio;
   }
 }
